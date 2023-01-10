2594cde9b0dc88062bfcbf4ab3e9ece3f4d14ff6
Toxgen <122239725+Toxgen@users.noreply.github.com>
2023-01-10 18:46:27 -0500

    Update first.py

--- first.py
+++ first.py
@@ -5,25 +5,27 @@ weapons = []
 items = []
 exp = []
 
-global monsterHp
-monsterHp = 10
-global selfHp
-selfHp = 50
+def Turtorial():
 
-def beginning():
-    time.sleep(1)
-    print('You have woken on your bed in a peaceful cabin')
-    time.sleep(1.5)
-    print('Today your gonna get your adventure badge!')
-    time.sleep(2)
-    print('But then, you find yourself between a monster')
-    time.sleep(2)
-    print('You know that you haven\'t gotten your badge yet, but you still attack it')
-    time.sleep(2.785)
-    print('The monster has ' + str(monsterHp) + ' hp')
-    time.sleep(3)
-    print('While you have ' + str(selfHp) + ' hp')
-beginning()
+    global monsterHp
+    monsterHp = 10
+    global selfHp
+    selfHp = 50
+
+    def beginning():
+        time.sleep(1)
+        print('You have woken on your bed in a peaceful cabin')
+        time.sleep(1.5)
+        print('Today your gonna get your adventure badge!')
+        time.sleep(2)
+        print('But then, you find yourself between a monster')
+        time.sleep(2)
+        print('You know that you haven\'t gotten your badge yet, but you still attack it')
+        time.sleep(2.785)
+        print('The monster has ' + str(monsterHp) + ' hp')
+        time.sleep(3)
+        print('While you have ' + str(selfHp) + ' hp')
+    beginning()
 
 def first_attack():
 
@@ -39,12 +41,12 @@ first_attack()
 def new_func():
     return 0
 
-fist_Dmg = random.randint(1, 10)
-monster_Dmg = random.randint(1, 5)
 
 #crtl^c to stop input line :/
 
 def attackSystem():
+  fist_Dmg = random.randint(1, 3)
+  monster_Dmg = random.randint(2, 4)
   global monsterHp, selfHp
   monsterHp = monsterHp - fist_Dmg
   selfHp = selfHp - monster_Dmg
@@ -59,14 +61,20 @@ def combat():
   counter = new_func()
   if counter == 0:
         while True:
-            time.sleep(0.275212)
+            time.sleep(0.25)
             line = input('> ')
-            if line.lower().strip() == 'attack' :
+            if line.lower().strip() == 'attack' or 'z' :
                 attackSystem()
             else:
                 print('That is not "attack", try again')
 
-while monsterHp >= 0:
-  combat()
-
-#if monsterHp <=0:
+while monsterHp != 0 or -1 or -2:
+    combat()
+    if monsterHp == 0:
+        break
+    print('Congratulations, you have defeated the monster')
+    time.sleep(0.6667)
+    print('Gotten "wooden cub"')
+    time.sleep(0.333)
+    weapons.append('wooden cub')
+    print('P.S. the "wooden cub does 3-5 DMG')

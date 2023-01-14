9214ba494d1111b9add28e81e807cc852450934e
Toxgen <122239725+Toxgen@users.noreply.github.com>
2023-01-13 21:08:29 -0500

    Update first.py

--- first.py
+++ first.py
@@ -14,33 +14,26 @@ global selfHp
 selfHp = 50
 
 def beginning():
-    time.sleep(1)
-    print('You have woken on your bed in a peaceful cabin')
+    print('The day is the September 1st, a peaceful and normal day')
     time.sleep(1.5)
-    print('Today your gonna get your adventure badge!')
+    print('You wake up and feel refresed from yesterday')
+    time.sleep(1.7666)
+    print('You get ready for your morning stroll around the woods')
     time.sleep(2)
-    print('But then, you find yourself between a monster')
-    time.sleep(2)
-    print('You know that you haven\'t gotten your badge yet, but you still attack it')
+    print('However, when you went outside, you saw a disgusting, huge creature')
     time.sleep(2.785)
+    print('The mouth was full of teeth, which were stained yellow, and standing up to 8 feet')
+    time.sleep(2)
+    print("You didn't carry anything, unfortunately, but you still tried to attack it")
     print('The monster has ' + str(monsterHp) + ' hp')
-    time.sleep(3)
+    time.sleep(4.5)
     print('While you have ' + str(selfHp) + ' hp')
-beginning()
-
-
-def first_attack():
-
     time.sleep(3.1)
     print('You will always attack first no matter what')
     time.sleep(2)
     print('In order to atack, you need to type "attack"')
 
-
-first_attack()
-
-# Maybe add description or more things to make the text sound more 'interesting'
-
+beginning()
 
 # crtl^c to stop input line :/
 
@@ -80,7 +73,8 @@ def combat():
                 break
 combat()
 
-# somehow make if the value of monsterHp goes neg, then ignore everying printed.
+# somehow make if the value of monsterHp goes neg, then ignore everying printed 
+# make exp a variable
 
 bruh()
 

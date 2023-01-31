d1255f9f0a3579139123d64bf8dda7007c5a6089
Toxgen <122239725+Toxgen@users.noreply.github.com>
2023-01-31 07:20:02 -0500

    Update first.py

--- first.py
+++ first.py
@@ -11,8 +11,8 @@ monsterHp = 10
 global selfHp
 selfHp = 50
 
-def beginning():
-    print('The day is the September 1st, a peaceful and normal day')
+def beginning(): # make the time even longer as Zach can't read that fast 
+    print('The day is the September 1st, a peaceful and normal day') # also is put like some pet with him
     time.sleep(1.5)
     print('You wake up and feel refresed from yesterday')
     time.sleep(1.7666)

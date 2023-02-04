9100153f48b23120f2525fb2ed9fb7ca7f9847eb
Toxgen <122239725+Toxgen@users.noreply.github.com>
2023-02-03 21:39:03 -0500

    Update first.py

--- first.py
+++ first.py
@@ -1,6 +1,11 @@
 import random
 import time
 
+'''
+"code is unreachable pylance" do: check if the code is after a return statement and
+                              put it before the return function-ish
+
+'''
 global weapons
 weapons = []
 items = []
@@ -11,48 +16,33 @@ monsterHp = 10
 global selfHp
 selfHp = 50
 
-def beginning(): 
-    print('The day is the September 1st, a peaceful and normal day') # also is put like some pet with him
-    time.sleep(2)
-    print('You wake up and feel refresed from yesterday')
-    time.sleep(2.2)
-    print('You get ready for your morning stroll around the woods')
-    time.sleep(2.2)
-    print('However, when you went outside, you saw a disgusting, huge creature')
-    time.sleep(3)
-    print('The mouth was full of teeth, which were stained yellow, and standing up to 8 feet')
-    time.sleep(3.1)
-    print("You didn't carry anything, unfortunately, but you still tried to attack it")
+
+def beginning():  # make the time even longer as Zach can't read that fast
+    # also is put like some pet with him
+    print('The day is the September 1st, a peaceful and normal day')
     time.sleep(1.5)
+    print('You wake up and feel refresed from yesterday')
+    time.sleep(1.7666)
+    print('You get ready for your morning stroll around the woods')
+    time.sleep(2)
+    print('However, when you went outside, you saw a disgusting, huge creature')
+    time.sleep(2.785)
+    print('The mouth was full of teeth, which were stained yellow, and standing up to 8 feet')
+    time.sleep(2)
+    print("You didn't carry anything, unfortunately, but you still tried to attack it")
+    time.sleep(1.12)
     print('The monster has ' + str(monsterHp) + ' hp')
-    time.sleep(3)
-    print('While you have ' + str(selfHp) + ' hp')
     time.sleep(2.75)
+    print('While you have ' + str(selfHp) + ' hp')
+    time.sleep(3.1)
     print('You will always attack first no matter what')
-    time.sleep(2.1)
+    time.sleep(2)
     print("\n")
     print('In order to attack, you need to type "attack"')
 
+
 beginning()
 
-# crtl^c to stop input line :/
-
-def attackSystem():
-    global monsterHp, selfHp
-    fist_Dmg = random.randint(1, 3)
-    monster_Dmg = random.randint(2, 4)
-    monsterHp = monsterHp - fist_Dmg
-    selfHp = selfHp - monster_Dmg
-    for i in range(1):
-        if not monsterHp == [0, -1, -2] or not monsterHp > 0:
-            time.sleep(0.2)
-            print('\n')
-            print('The monster now has, ' + str(monsterHp) + ' hp')
-            time.sleep(1)
-            print('Ouch, now you have, ' + str(selfHp) + ' hp')
-            return monsterHp, selfHp
-        else:
-            break
 
 def bruh():
     print('Congratulations, you have defeated the monster')
@@ -64,28 +54,49 @@ def bruh():
     print('Gained 8 XP')
     print('\n')
 
-def combat():
-    if monsterHp > 0:
-         while True:
-            time.sleep(0.25)
-            line = input('> ')
-            if line.lower().strip() == 'attack' :
-                    attackSystem()
-            else:
-                print('That is not "attack", try again')
-            
-            if monsterHp < 0:
-                break
-combat()
+# crtl^c to stop input line :/
 
+
+def attackSystem():
+    global monsterHp, selfHp
+    fist_Dmg = random.randint(1, 3)
+    monster_Dmg = random.randint(2, 4)
+    monsterHp -= fist_Dmg
+    selfHp -= monster_Dmg
+    while monsterHp >= 0:
+        time.sleep(0.2)
+        print('\n')
+        # do if statement if tghey dont like that it is printing out that the mhp is 0
+        print('The monster now has, ' + str(monsterHp) + ' hp')
+        time.sleep(1)
+        print('Ouch, now you have, ' + str(selfHp) + ' hp')
+        return monsterHp, selfHp
+bruh()
+
+
+def combat():
+    while monsterHp >= 0:
+        if monsterHp == 0:
+            break
+        if monsterHp < 0:
+            break
+        time.sleep(0.25)
+        line = input('> ')
+        if line.lower().strip() == 'z':
+            attackSystem()
+        else:
+            print('That is not "adv", try again')
+
+
+combat()
 bruh()
 
 weapons.append("wooden cub")
-Exp =+ 8
+Exp = + 8
 
 
 def Sad_goodbye():
-    print('\x1b[3;30;40m' + 'A few minute later...' + '\x1b[0m') 
+    print('\x1b[3;30;40m' + 'A few minute later...' + '\x1b[0m')
     time.sleep(2.3)
     print('You got back to your cabin, preparing for the long adventure that awaits you to the city')
     time.sleep(4)
@@ -93,6 +104,7 @@ def Sad_goodbye():
     time.sleep(3)
     print('After you\'re done, you say goodbye to your small cabin and started your journey')
     print('\n')
+
 
 Sad_goodbye()
 
@@ -195,13 +207,15 @@ print('What do you want to do, ' + name)
 
 print('\n')
 time.sleep(1.2223)
-# put like a print statement for like "oh you dont know how to do things"
+
 print('Type in ".help" to find your commands')
-def bk(): 
+
+
+def bk():
     global commands
-    help1 = input('> ') 
-    while help1.lower().strip() == ".help" :
-        if help1.lower().strip() == ".help": 
+    help1 = input('> ')
+    while help1.lower().strip() == ".help":
+        if help1.lower().strip() == ".help":
             commands = ['stats', 'inv', 'adv']
             print(*commands, sep='\n')
             time.sleep(0.34)
@@ -211,15 +225,18 @@ def bk():
         print('\n')
         bk()
 
+
 bk()
 
 time.sleep(1)
 print('Try to type one of the printed words out!!')
-time.sleep(1)
+time.sleep(0.2)
 
 help2 = input('> ')
+
+
 def picking101():
-    while help2 != None: # maybe change this to len, test this out.
+    while help2 != None:
         if help2.lower().strip() == commands[0]:
             print('Here are your stats')
             break
@@ -232,5 +249,6 @@ def picking101():
         else:
             print('type in one of the above words ("stats", "inv", "adv")')
             picking101()
+
 
 picking101()

4b2fd9677557a6aede2864f45d1e4b21e4a017b8
Toxgen <Hydro_Toxgen@outlook.com>
2023-03-22 22:48:06 -0400

    Helping a friend out on a project

--- p4ye/testing_area.1.py
+++ p4ye/testing_area.1.py
@@ -1,189 +1,148 @@
-print("try")
-
-
-'''
+import turtle
 import time
-import random
+# Setup
+# wn stands for window
+wn = turtle.Screen()
+wn.title("Pong by Jayden Xia and Ethan Zhang")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center",
+          font=("Courier", 24, "normal"))
 
-global weapons
-inv = dict()
-items = dict()
-weapons = 1
-weaponsdict = {
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.color("white")
+paddle_a.shape("square")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
 
-    "weapon(s)": weapons,
-}
-exp = 0
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
 
-inv = 
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("white")
+ball.shapesize(stretch_wid=1, stretch_len=1)
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 0.1
+ball.dy = -0.1
 
-global monsterHp
-monsterHp = 10
-global selfHp
-selfHp = 50
+# Functions
 
-weapons.update({"wooden club": 1})
-Exp = + 8
 
-def naming():
-    global name
-    print("Make a Username")
-    time.sleep(2.2)
-    print('The Username can only be 1-8 characters long')
-    name = input('> ')
-    dummy = len(name)
-    while dummy >= 9:
-        print('\n')
-        print('3-8 characters')
-        time.sleep(1.0)
-        print('Try again')
-        name = input('> ')
-        dummy = len(name)
-        if dummy <= 9:
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
+wn.listen()
+# Moving the paddle
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+# Main game loop
+score_a = 0
+score_b = 0
+
+def game():
+    global score_a, score_b 
+    while True:
+        wn.update()
+
+        # Moving the ball
+        ball.setx(ball.xcor() + ball.dx)
+        ball.sety(ball.ycor() + ball.dy)
+
+        # Border checking
+        if ball.ycor() > 290:
+            ball.sety(290)
+            ball.dy *= -1
+
+        if ball.ycor() < -290:
+            ball.sety(-290)
+            ball.dy *= -1
+
+        if ball.xcor() > 390:
+            ball.goto(0, 0)
+            ball.dx *= -1
+            score_a += 1
+            pen.clear()
+            pen.write("Player A: {} Player B: {}".format(score_a, score_b),
+                    align="center", font=("Courier", 24, "normal"))
+
+        if ball.xcor() < -390:
+            ball.goto(0, 0)
+            ball.dx *= -1
+            score_b += 1
+            pen.clear()
+            pen.write("Player A: {} Player B: {}".format(score_a, score_b),
+                    align="center", font=("Courier", 24, "normal"))
+
+        # Paddle and ball collisions
+        if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b. ycor() - 50):
+            ball.setx(340)
+            ball.dx *= -1
+
+        if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a. ycor() - 50):
+            ball.setx(-340)
+            ball.dx *= -1
+
+        # Score
+
+        if score_a == 1:
+            time.sleep(0.45)
+            pen.goto(0, 0)
+            pen.write("Player A Wins", align="center",
+                    font=("Courier", 24, "normal"))
+            time.sleep(10) # change this value to set for how long the screen appears for
             break
-    if len(name) != 0:
-        print('\n')
-        print('You typed in "' + name + '". Are you Sure? Type in "yes" or "no"')
-        Sureness = input('> ')
-        if Sureness.lower().strip() == 'yes':
-            print('Your name now will be "' + name + '" ')
-
-        elif Sureness.lower().strip() != 'no':
-           while Sureness.lower().strip() != 'yes':
-            print('Type in Yes or No')
-            Sureness = input('> ')
-            if Sureness.lower().strip() == 'yes':
-                print('Your name is "' + name + '"')
-                break
-
-            elif Sureness.lower().strip() == 'no':
-                time.sleep(0.22)
-                print('\n')
-                while Sureness.lower().strip() == 'no':
-                    print("What's your new name")
-                    name = input('> ')
-                    dummy = len(name)
-                    while dummy >= 9:
-                        print('\n')
-                        print('3-8 characters')
-                        time.sleep(1.0)
-                        print('Try again')
-                        name = input('> ')
-                        dummy = len(name)
-                        if dummy <= 9:
-                            break
-                    if len(name) != 0:
-                        print('You typed in "' + name +
-                              '". Are you Sure? Type in "yes" or "no"')
-                        Sureness = input('> ')
-                        if Sureness.lower().strip() == 'yes':
-                            print('Your name now will be "' + name + '" ')
-                            break
-                        else:
-                            print('')
-            else:
-                print('\n')
-
-        else:
-            time.sleep(0.22)
-            while Sureness.lower().strip() == 'no':
-                print('\n')
-                print("What's your new name ")
-                name = input('> ')
-                dummy = len(name)
-                while dummy >= 9:
-                    print('\n')
-                    print('3-8 characters')
-                    time.sleep(1.0)
-                    print('Try again')
-                    name = input('> ')
-                    dummy = len(name)
-                    if dummy <= 9:
-                        break
-                if len(name) != 0:
-                    print('You typed in "' + name +
-                          '". Are you Sure? Type in "yes" or "no"')
-                    Sureness = input('> ')
-                    if Sureness.lower().strip() == 'yes':
-                        print('Your name is "' + name + '" ')
-                        break
-                    else:
-                        print('')
-                else:
-                    print('\n')
-                    naming()
-    else:
-        print('\n')
-        naming()
-
-
-naming()
-
-print('What do you want to do, ' + name)
-
-print('\n')
-time.sleep(1.2223)
-
-print('Type in "help" to find your commands')
-
-
-def bk():
-    global commands
-    help1 = input('> ')
-    while help1.lower().strip() == "help":
-        if help1.lower().strip() == "help":
-            commands = ['stats', 'inv', 'adv']
-            print(*commands, sep='\n')
-            time.sleep(0.34)
-            break
-    else:
-        print('type in "help"')
-        print('\n')
-        bk()
-
-bk()
-
-time.sleep(1)
-print('Try to type one of the printed words out!!')
-time.sleep(0.2)
-
-def picking101():
-    help2 = input('> ')
-    if help2.lower().strip() == commands[0]:
-        print('Here are your stats')
-        stats = dict()
-        stats = {
-            "name": name,
-            "exp": exp,
-            "health": selfHp
-        }
-        print(sep=' ')
-        for key, value in stats.items():
-            if key == 'name':
-                    print('------------------------')
-                    print(key + ':')
-                    print('"' + value + '" ')
-                    print('------------------------')
-            elif key == 'exp':
-                    print(key + ':')
-                    print(str(value) + '/10')
-                    print('------------------------')
-            elif key == 'health':
-                    print(key + ':')
-                    print(str(value) + '/50')
-                    print('------------------------')
-                    break
-    elif help2.lower().strip() == commands[1]:
-        print('Here is your inventory')
-        for key, value in inv.items():
         
-    elif help2.lower().strip() == commands[2]:
-        print('WORK IN PROGRESS')
-    else:
-        print('\n')
-        print('Type in one of the words above ("stats", "inv", "adv")')
-        picking101()
-
-
-picking101()
-'''
+        else:
+            if score_b == 1:
+                time.sleep(0.45)
+                pen.goto(0, 0)
+                pen.write("Player B Wins", align="center",
+                        font=("Courier", 24, "normal"))
+                time.sleep(10) # change this value to set for how long the screen appears for
+                break
+            
+game() # this is for if you want to make like a new game button or smth like that

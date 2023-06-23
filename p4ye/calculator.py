4d65a64c9663777968cb4118733f410baab835b6
Toxgen <Hydro_Toxgen@outlook.com>
2023-06-22 22:59:51 -0400

    Reworked Calculator Code

--- p4ye/calculator.py
+++ p4ye/calculator.py
@@ -1,138 +1,130 @@
 import time as t
 
-def calulating(num, op) -> int:
-    return num, op
+print("just a calculator")
 
-def opsTranslate(list) -> list: 
-    for a in range(len(list)): 
-        if list[a] == 1:
-            list.pop(a)
-            list.insert(a, "+")
-        if list[a] == 2:
-            list.pop(a)
-            list.insert(a, "-")
-        if list[a] == 3:
-            list.pop(a)
-            list.insert(a, "*")
-        if list[a] == 4:
-            list.pop(a)
-            list.insert(a, "/")
-    return list
+num1 = 0 # Declaring Variables
+num2 = 0
+ans = 0
+continueOperation = None
 
-def calculator() -> int:
-    list_num = []
-    ops = []
-
-    print("How Many Numerical Values Would You Like In Your Calculation? *fyi, up to 10") 
-
-    while True: 
-        amount = input("Amount: ")
-        try:
-            amt = int(amount)
+def add(x, y): # Addition
+    return x + y
+def sub(x, y): # Subtraction
+    return x - y
+def multi(x, y): # Multiplication
+    return x * y
+def divid(x, y): # Division
+    if x + y == 0:
+        return "Infinity"
+    else:
+        return x / y
+    
+def checkingEquation():
+    while True: # Main Loop
+        amount1 = input("Number 1: ") 
+        try: # Checking if input is int
+            num1 = int(amount1)
         except ValueError:
             print("Not int", '\n')
             continue
-        if amt <= 1:
-            print("Please Type in a Number Between 2 - 10", '\n')
+
+        amount2 = input("Number 2: ") 
+        try: # Checking if input is int
+            num2 = int(amount2)
+        except ValueError:
+            print("Not int", '\n')
             continue
-        if amt > 10:
-            print("Please Type Something Lower Than 11", '\n')
-        else:
-            print('\n')
-            break
 
-    for i in range(amt): 
-        while True:
-            userInput = input("Enter A Number: ") 
-            try:
-                num = int(userInput)
-            except ValueError:
-                print("Not int", '\n')
-                continue
-            else:
-                break
-        list_num.append(num)
+        print('\n')
 
-    print("""
-    In order to enter a operation, you have to enter a code for that specific operation
-
-    1 -> +
-    2 -> -
-    3 -> *
-    4 -> /
-    """, "\n")
-
-    t.sleep(3.9)
-
-    for x in range(amt-1):
-        while True:
-            userInput = input("Enter a Operation Code: ") 
-            try:
-                opss = userInput
-                if int(opss) > 5:
-                    raise Exception()
-                if int(opss) < 0:
-                    raise Exception()
+        while True: 
+            userInput = input("Operation: ") 
+            try: 
+                if userInput == "+":
+                    break
+                elif userInput == "-":
+                    break
+                elif userInput == "*":
+                    break
+                elif userInput == "/": 
+                    break
+                else:
+                    raise Exception
             except Exception:
-                print("Not Valid", '\n')
+                print("Not an Operation", '\n')
                 continue
-            else:
-                break
-        ops.append(int(opss))
 
-    print("Translating over...")
-    t.sleep(0.444)
-    opsTranslate(ops)
+        print(f"Does this Equation Look Right: {num1} {userInput} {num2}?", "y for yes, n for no", sep='\n') 
+    
+def calculator():
+    global continueOperation
 
-    try:
-        int2 = "{0} {1} {2}".format(
-            list_num[0], ops[0], list_num[1]) 
-        int3 = "{0} {1} {2} {3} {4}".format(
-            list_num[0], ops[0], list_num[1], ops[1], list_num[2])
-        int4 = "{0} {1} {2} {3} {4} {5} {6}".format(
-            list_num[0], ops[0], list_num[1], ops[1], list_num[2], ops[2], list_num[3])
-        int5 = "{0} {1} {2} {3} {4} {5} {6} {7} {8}".format(
-            list_num[0], ops[0], list_num[1], ops[1], list_num[2], ops[2], list_num[3], ops[3], list_num[4])
-        int6 = "{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10}".format(
-            list_num[0], ops[0], list_num[1], ops[1], list_num[2], ops[2], list_num[3], ops[3], list_num[4], 
-            ops[4], list_num[5])
-        int7 = "{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12}".format(
-            list_num[0], ops[0], list_num[1], ops[1], list_num[2], ops[2], list_num[3], ops[3], list_num[4], 
-            ops[4], list_num[5], ops[5], list_num[6])
-        int8 = "{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} {13} {14}".format(
-            list_num[0], ops[0], list_num[1], ops[1], list_num[2], ops[2], list_num[3], ops[3], list_num[4], 
-            ops[4], list_num[5], ops[5], list_num[6], ops[6], list_num[7])
-        int9 = "{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} {13} {14} {15} {16}".format(
-            list_num[0], ops[0], list_num[1], ops[1], list_num[2], ops[2], list_num[3], ops[3], list_num[4], 
-            ops[4], list_num[5], ops[5], list_num[6], ops[6], list_num[7], ops[7], list_num[8])
-        int10 = "{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} {13} {14} {15} {16} {17} {18}".format(
-            list_num[0], ops[0], list_num[1], ops[1], list_num[2], ops[2], list_num[3], ops[3], list_num[4], 
-            ops[4], list_num[5], ops[5], list_num[6], ops[6], list_num[7], ops[7], list_num[8], ops[8], list_num[9])
-    except Exception:
-        print("Declaring Variables...", '\n')
-        t.sleep(0.45)
-    
-    if amt == 2:
-        print(int2, '\n')
-    elif amt == 3:
-        print(int3, '\n')
-    elif amt == 4:
-        print(int4, '\n')
-    elif amt == 5:
-        print(int5, '\n')
-    elif amt == 6:
-        print(int6, '\n')
-    elif amt == 7:
-        print(int7, '\n')
-    elif amt == 8:
-        print(int8, '\n')
-    elif amt == 9:
-        print(int9, '\n')
-    elif amt == 10:
-        print(int10, '\n')
-    else:
-        raise Exception("Something Went Wrong, Retry")
-    
-    calulating(list_num, ops)
-    
+    while True: # Maybe add something to check if they put ans in the amount1 <===
+        amount1 = input("Number 1: ")
+        try: # Checking if input is int
+            num1 = int(amount1)
+        except ValueError:
+            print("Not int", '\n')
+            continue
+
+        amount2 = input("Number 2: ") 
+        try: # Checking if input is int
+            num2 = int(amount2)
+        except ValueError:
+            print("Not int", '\n')
+            continue
+
+        print("\n")
+        print("Please type in either '-', '+', '*', or '/'")
+
+        while True: 
+            userInput = input("Operation: ") 
+            try: 
+                if userInput == "+":
+                    break
+                elif userInput == "-":
+                    break
+                elif userInput == "*":
+                    break
+                elif userInput == "/": 
+                    break
+                else:
+                    raise Exception
+            except Exception:
+                print("Not an Operation", '\n')
+                continue
+
+        print(f"Does this Equation Look Right: {num1} {userInput} {num2}?", "y for yes, n for no", sep='\n') 
+
+        if userInput == "+": 
+            ans = add(num1, num2)
+        elif userInput == "-":
+            ans = sub(num1, num2)
+        elif userInput == "*":
+            ans = multi(num1, num2)
+        elif userInput == "/": 
+            ans = divid(num1, num2)
+            if ans == "Infinity":
+                raise Exception("Infinity cannot be added, subtracted, multiplied, or divided")
+
+        print(f"The ans is {ans}")
+        t.sleep(1.32)
+        break
+
 calculator()
+
+while True:
+    print("Continue Operation?: y for yes & n for no")
+    try:
+        continueOperation = input('> ').lower().strip()
+
+        if continueOperation == "y":
+            calculator()
+        elif continueOperation == "n":
+            break
+        else:
+            raise Exception()
+    except Exception:
+        print("Please type in y or n", sep='\n')
+        continue
+print("Calculation Ended")

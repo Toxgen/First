703c0f9690064b063179d9e3359602920048ff42
Toxgen <Hydro_Toxgen@outlook.com>
2023-07-04 23:24:36 -0400

    lol i hate programming

--- p4ye/game.py
+++ p4ye/game.py
@@ -2,25 +2,28 @@ import random as r
 import time as t
 
 """
-TYPE IN YES TWICE QUICK: DIDNT REGISTER SOMEHOW I DUNNO
+ADD SOME KIND OF FOOD SYSTEM PROB, AND BUGTEST THIS POOP PROGRAM
 """
 
 
 
 class game:
-    def __init__(self, hp, defe, mobHp, mobAttk, mobDefe, luck, input, name, addingWep, mobList, inv, weapDict):
+    def __init__(self, hp=20, defe=1, mobHp=0, mobAttk=0, mobDefe=0, crit_chance=10, crit_dmg=1.5, input='', name='', addingWep=[], mobList=[], inv={}, weapDict={}):
         self.hp = hp
         self.defe = defe
         self.mobHp = mobHp
         self.mobAttk = mobAttk
         self.mobDefe = mobDefe
-        self.luck = luck
+        self.crit_chance = crit_chance
+        self.crit_dmg = crit_dmg
         self.input = input
         self.name = name
         self.addingWep = addingWep
         self.mobList = mobList
+        self.mobList.append("goblin")
         self.inv = inv
         self.weapDict = weapDict
+        self.weapDict['fist'] = r.randint(1, 3)
 
 
     def naming(self):
@@ -77,6 +80,7 @@ class game:
                 
     def help_ccmd(self):
         print("Type In cmd For Commands & stop to stop", "\n")
+        t.sleep(0.5)
         while True:
             self.input = input('> ').lower().strip()
             if self.input == "cmd":
@@ -85,47 +89,43 @@ class game:
                         'Stats': To show your stats
                         'Inv': To show your inventory
                         'Adv': To start an adventure 
+                        'Swi': To switch current weapon
                         """)
                 t.sleep(1.0)
+                continue
             if self.input == "stats":
                 print("yay")
             elif self.input == "inv":
-               for key, value in self.inv:
+                for key, value in self.inv:
                    print(f'{key}:{value}')
+                   t.sleep(0.075)
                    if key // 2 == float:
                        print('\n')
                    else:
                        print(sep='   ')
+                break
             elif self.input == "adv":
-                print("YAY")
+                self.adventure(self, 1)
+                break
             else: 
-                if self.input == 'cmd':
-                    continue
-                else:
-                    print("Please type in a allowed command", '\n', self.input)
+                print("Please type in a allowed command", '\n')
                 continue
 
     def attk_RNGESUS(self):
-        print("Which weapons would you like to attack with?", self.weapDict.keys(), sep='\n', end='\n') # scoot this down to the adventure(self)
-        while True:
-            self.input = input('> ').lower().strip()
-            if self.input == 'fist' and self.addingWep >= 0:
-                print("ok")
-                break
-            elif self.input == "stick":
-                if self.addingWep >= 1:
-                    print("ok")
-                    break
-                else:
-                    print("you have yet to obtain this yet, retry", '\n')
-                    continue
-            else:
-                print("Type in a valid weapon")
-                
-        return self.weapDict.get(self.input) + r.randint(self.luck, self.luck + 3) - self.mobDefe  # Maybe add something like critcal hit 
+        if self.crit_chance == 100:
+            return self.weapDict.get(self.input) * self.crit_dmg - self.mobDefe
+        for i in range(1 + self.crit_chance):
+            chance = r.randint(1, 100)
+            if chance == self.crit_chance:
+                return self.weapDict.get(self.input) * self.crit_dmg - self.mobDefe
+        
+        return self.weapDict.get(self.input) - self.mobDefe 
     
-    def defe_RNGESUS(self):
+
+    def defe_RNGESUS(self): # Add critcal chance for mob?
         return self.mobAttk - self.defe
+    
+    
     def addingWeapons(self):
         if self.addingWep == 0:
             self.weapDict['fist'] = 2
@@ -133,17 +133,17 @@ class game:
             self.weapDict['stick'] = 3
         else:
             pass
+
+
     def adventure(self, time):
+        
+        
         atk_RNGESUS = 0
         dee_RNGESUS = 0
-        self.mobList.append("goblin") # add all enemies
-        self.weapDict['fist'] = r.randint(1, 3)
-        if time == 0:
-            for q in range(len(self.mobList)):
-                if 'goblin' in self.mobList[q]:
-                    self.mobHp = 10
-                    self.mobAttk = r.randint(1,3)
-                    self.mobDefe = 0
+        if time:
+            self.mobHp = 10
+            self.mobAttk = r.randint(1,3)
+            self.mobDefe = 0
 
             print("New Adventure!", '\n')
             print(f"Encountered 'Goblin'! || Hp: {self.mobHp}, Attk: {self.mobAttk}, Def: {self.mobDefe}") # For encountered goblin make some system that appends that enemy bc of that
@@ -153,15 +153,28 @@ class game:
                 if self.input == "attack":
                     atk_RNGESUS = game.attk_RNGESUS(self)
                     self.mobHp -= atk_RNGESUS
-                    print(f"The goblin health is at {self.mobHp}")
+                    if self.mobHp <= 0:
+                        break
+                    else:
+                        print(f"The goblin health is at {self.mobHp}")
                     dee_RNGESUS = game.defe_RNGESUS(self)
                     self.hp -= dee_RNGESUS
-                    print(f"Oh no, your health is at {self.hp}")
+                    if self.hp <= 0:
+                        break
+                    else:
+                        print(f"Oh no, your health is at {self.hp}")
                 else:
                     print("Please type in attack", '\n')
+                
+                print("You have defeated the Goblin!")
+                # Add some loot system here <------------------------- god so much work?
+        else:
+            pass # Add what after tutorial will look like
+
+
 
             
-gaming = game(20, 1, 0, 0, 0, 0, '', '', 0, list(), dict(), dict())
+gaming = game()
 
 def start():
     print('u live in cabin and want explore', 'choose name', '\n', sep='\n')
@@ -169,6 +182,6 @@ def start():
     print(f'{name}, nice name, now go out in the world!')
     t.sleep(2.5)
     print("Type in adv to start an adventure!")
-    gaming.adventure(0)
+    gaming.adventure(1)
 start()
 

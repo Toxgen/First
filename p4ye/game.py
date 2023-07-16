4c96cba6814ae59344b7caf5a12750a3b8db7208
Toxgen <Hydro_Toxgen@outlook.com>
2023-07-15 23:35:36 -0400

    lolololololol

--- p4ye/game.py
+++ p4ye/game.py
@@ -8,22 +8,26 @@ ADD SOME KIND OF FOOD SYSTEM PROB, AND BUGTEST THIS POOP PROGRAM
 
 
 class game:
-    def __init__(self, hp=20, defe=1, mobHp=0, mobAttk=0, mobDefe=0, crit_chance=10, crit_dmg=1.5, input='', name='', addingWep=[], mobList=[], inv={}, weapDict={}):
-        self.hp = hp
-        self.defe = defe
+    hp = 20
+    defe = 0
+    crit_chance = 10
+    crit_dmg = 1.5
+    luck = 0
+    input = ''
+    weaprn = ''
+    mob = ''
+
+    weapDict = {}
+
+    def __init__(self, mobHp=0, mobAttk=0, mobDefe=0, name='', addingWep=[], mobList=[], inv={}):
         self.mobHp = mobHp
         self.mobAttk = mobAttk
         self.mobDefe = mobDefe
-        self.crit_chance = crit_chance
-        self.crit_dmg = crit_dmg
-        self.input = input
         self.name = name
         self.addingWep = addingWep
         self.mobList = mobList
         self.mobList.append("goblin")
         self.inv = inv
-        self.weapDict = weapDict
-        self.weapDict['fist'] = r.randint(1, 3)
 
 
     def naming(self):
@@ -32,15 +36,15 @@ class game:
         if len(self.name) > 0:
             print("Rename?", "Type in no or yes", sep='\n')
             while True:
-                self.input = input('> ').lower().strip()
-                if self.input == "yes":
+                gameinput = input('> ').lower().strip()
+                if game.input == "yes":
                     break
-                elif self.input == "no":
-                    self.input = True
+                elif game.input == "no":
+                    game.input = True
                 else:
                     print("Please Type in yes or no", '\n')
 
-        if self.input == True:
+        if game.input == True:
             pass
         t.sleep(1.5)
         print("Name?", "p.s. 3 - 10 characters long & no special characters", sep='\n')
@@ -67,10 +71,10 @@ class game:
                     break
 
         while True:
-            self.input = input('> ').lower().strip()
-            if self.input == "yes":
+            game.input = input('> ').lower().strip()
+            if game.input == "yes":
                 return self.name
-            elif self.input == "no":
+            elif game.input == "no":
                 self.name = ''; print('\n')
                 game.naming(self)
             else:
@@ -82,8 +86,8 @@ class game:
         print("Type In cmd For Commands & stop to stop", "\n")
         t.sleep(0.5)
         while True:
-            self.input = input('> ').lower().strip()
-            if self.input == "cmd":
+            game.input = input('> ').lower().strip()
+            if game.input == "cmd":
                 print("""The Following Commands Are:
 
                         'Stats': To show your stats
@@ -93,9 +97,9 @@ class game:
                         """)
                 t.sleep(1.0)
                 continue
-            if self.input == "stats":
+            if game.input == "stats":
                 print("yay")
-            elif self.input == "inv":
+            elif game.input == "inv":
                 for key, value in self.inv:
                    print(f'{key}:{value}')
                    t.sleep(0.075)
@@ -104,7 +108,7 @@ class game:
                    else:
                        print(sep='   ')
                 break
-            elif self.input == "adv":
+            elif game.input == "adv":
                 self.adventure(self, 1)
                 break
             else: 
@@ -112,28 +116,40 @@ class game:
                 continue
 
     def attk_RNGESUS(self):
-        if self.crit_chance == 100:
-            return self.weapDict.get(self.input) * self.crit_dmg - self.mobDefe
-        for i in range(1 + self.crit_chance):
+        if game.crit_chance == 100:
+            return game.weapDict.get(game.input) * game.crit_dmg - self.mobDefe
+        for i in range(1 + game.crit_chance):
             chance = r.randint(1, 100)
-            if chance == self.crit_chance:
-                return self.weapDict.get(self.input) * self.crit_dmg - self.mobDefe
-        
-        return self.weapDict.get(self.input) - self.mobDefe 
+            if chance == game.crit_chance:
+                return game.weapDict.get(game.input) * game.crit_dmg - self.mobDefe
+            else:
+                print("No lucky...")
+        return game.weapDict.get(game.input) - self.mobDefe 
     
 
-    def defe_RNGESUS(self): # Add critcal chance for mob?
-        return self.mobAttk - self.defe
+    def defe_RNGESUS(self): 
+        return self.mobAttk - game.defe
     
     
     def addingWeapons(self):
         if self.addingWep == 0:
-            self.weapDict['fist'] = 2
+            game.weapDict['fist'] = 2
         elif self.addingWep == 1:
-            self.weapDict['stick'] = 3
+            game.weapDict['stick'] = 3
         else:
             pass
-
+        
+    def selectWeapon(self):
+            
+        print(f"""_______________________________
+                    {game.weapDict.get("Fist", '')} 
+                    {game.weapDict.get("Stick", 'Not Yet Unlocked!')}
+                  ________________________________""")
+            
+    def lootTables(self):
+        if game.mob == 'goblin':
+            for i in range(10+game.luck):
+           #     self.inv.append("Place holder", 6) # This will defintely add a error
 
     def adventure(self, time):
         
@@ -144,13 +160,14 @@ class game:
             self.mobHp = 10
             self.mobAttk = r.randint(1,3)
             self.mobDefe = 0
-
+            self.selectWeapon()
             print("New Adventure!", '\n')
             print(f"Encountered 'Goblin'! || Hp: {self.mobHp}, Attk: {self.mobAttk}, Def: {self.mobDefe}") # For encountered goblin make some system that appends that enemy bc of that
             print("Type attack to attack your opponent!")
+            game.mob = "goblin"
             while True:
-                self.input = input('> ').lower().strip()
-                if self.input == "attack":
+                game.input = input('> ').lower().strip()
+                if game.input == "attack":
                     atk_RNGESUS = game.attk_RNGESUS(self)
                     self.mobHp -= atk_RNGESUS
                     if self.mobHp <= 0:
@@ -158,13 +175,15 @@ class game:
                     else:
                         print(f"The goblin health is at {self.mobHp}")
                     dee_RNGESUS = game.defe_RNGESUS(self)
-                    self.hp -= dee_RNGESUS
-                    if self.hp <= 0:
+                    game.hp -= dee_RNGESUS
+                    if game.hp <= 0:
                         break
                     else:
                         print(f"Oh no, your health is at {self.hp}")
+                        continue
                 else:
                     print("Please type in attack", '\n')
+                    continue
                 
                 print("You have defeated the Goblin!")
                 # Add some loot system here <------------------------- god so much work?

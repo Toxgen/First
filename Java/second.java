982359b4dff76d51f29e1604928fab0ec650bc73
Toxgen <Hydro_Toxgen@outlook.com>
2023-08-10 19:37:04 -0400

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- Java/second.java
+++ Java/second.java
@@ -1,9 +1,9 @@
 package Java;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class second {
     public static void main(String[] args) {
-        ArrayList<Integer> a = new ArrayList<Integer>();
+       // ArrayList<Integer> a = new ArrayList<Integer>();
     }
 }

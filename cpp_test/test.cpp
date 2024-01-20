7791f7146c356cb2fc9a858e250164cb7bf1aa64
Toxgen <Hydro_Toxgen@outlook.com>
2024-01-19 22:47:55 -0500

    what am i doing with my life

---
+++ cpp_test/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+using cap = int;
+using wow = double;
+
+cap main() 
+{
+    cap something = 10;
+    printf("mutliplied = %d", something * something);
+    return 0;
+}
